dc92a48a5b2f39de315751728a1c8c7a2e1c08d1
Annalise Mariottini <annalise.mariottini@columbia.edu>
2021-04-14 21:34:09 -0400

    moved tests, created 'fail' folder

--- testall.sh
+++ testall.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-for t in ./test/*.cll; do
+echo "***********************************"
+echo "THESE TESTS SHOULD PASS"
+echo "***********************************"
+for t in ./test/pass/*.cll; do
+    output="$(./cll.native -s $t)"
+    echo "***********************************"
+    echo "TESTING FILE: ${t}"
+    echo "***********************************"
+    echo "${output}"
+    echo "***********************************"
+    echo ""
+done
+
+echo "***********************************"
+echo "THESE TESTS SHOULD FAIL"
+echo "***********************************"
+for t in ./test/fail/*.cll; do
     output="$(./cll.native -s $t)"
     echo "***********************************"
     echo "TESTING FILE: ${t}"
