58c33a8acb9edf71b7c915ecc0d5c409c3f23014
Annalise Mariottini <annalise.mariottini@columbia.edu>
2021-04-23 14:35:42 -0400

    list accomodates list and string

--- bin/testall.zsh
+++ bin/testall.zsh
@@ -11,23 +11,23 @@ for t in ./test/*.cll; do
     echo "$to_stdout"
     output+="${to_stdout}\n"
     output+="***********************************\n"
-    output+="$(./cll.native -s $t)\n"
+    output+="$(./cll.native $t)\n"
     output+="***********************************\n"
     output+="\n"
 done
 
-output+="***********************************\n"
-output+="THESE TESTS SHOULD FAIL\n"
-output+="***********************************\n"
-output+="\n"
-for t in ./test/fail/*.cll; do
-    output+="***********************************\n"
-    to_stdout="TESTING FILE: ${t}"
-    echo "$to_stdout"
-    output+="${to_stdout}\n"
-    output+="***********************************\n"
-    output+="$(./cll.native -s $t)\n"
-    output+="***********************************\n"
-    output+="\n"
-done
+#output+="***********************************\n"
+#output+="THESE TESTS SHOULD FAIL\n"
+#output+="***********************************\n"
+#output+="\n"
+#for t in ./test/fail/*.cll; do
+#    output+="***********************************\n"
+#    to_stdout="TESTING FILE: ${t}"
+#    echo "$to_stdout"
+#    output+="${to_stdout}\n"
+#    output+="***********************************\n"
+#    output+="$(./cll.native -s $t)\n"
+#    output+="***********************************\n"
+#    output+="\n"
+#done
 echo $output > test.out
--- codegen.ml
+++ codegen.ml
@@ -20,6 +20,8 @@ module StringMap = Map.Make(String)
 
 (* translate : Sast.program -> Llvm.module *)
 let translate env sast =
+    let list_types = Hashtbl.create 10 in
+    let dict_types = Hashtbl.create 10 in
     let not_impl = " not implemented" in
     let context = L.global_context () in
 
@@ -33,18 +35,30 @@ let translate env sast =
     in let i8_t       = L.i8_type     context
     in let float_t    = L.double_type context
     in let string_t   = L.pointer_type i8_t
-    in let list_t     = L.named_struct_type context "list"
     in let void_t     = L.void_type   context
     in
 
     (* Return the LLVM type for a MicroC type *)
-    let typ_to_ltyp = function
+    let rec typ_to_ltyp = function
           A.Int    -> i32_t
         | A.Bool   -> i1_t
         | A.Float  -> float_t
         | A.String -> string_t
         | A.Null   -> void_t
-        | A.List(_)-> list_t
+        | A.List(t)->
+            let ltyp = typ_to_ltyp t in
+            let ltyp_s = List.hd (String.split_on_char ' ' (L.string_of_lltype ltyp)) in
+            let list_name = "list" ^ ltyp_s in
+            if Hashtbl.mem list_types list_name then (
+                let list_t = Hashtbl.find list_types list_name in
+                list_t
+            ) else (
+                let list_t = L.named_struct_type context list_name in
+                L.struct_set_body list_t [| L.pointer_type ltyp; L.pointer_type list_t |] false;
+                Hashtbl.add list_types list_name list_t;
+                list_t
+            )
+
         (*
         | A.UserTyp(ut)  -> let (_, at) = StringMap.find ut env.tsym in ltype_of_typ at
         *)
@@ -72,7 +86,7 @@ let translate env sast =
     let str_format = L.build_global_stringptr "%s\n" "fmt" builder in
 
     let rec expr builder = function
-          SIntLit(i) -> L.const_int i32_t i
+        SIntLit(i) -> L.const_int i32_t i
         | SFloatLit(f) -> L.const_float_of_string float_t f
         | SBoolLit(b) -> L.const_int i1_t (if b then 1 else 0)
         | SStrLit(s) ->
@@ -90,39 +104,41 @@ let translate env sast =
             ignore(L.build_store (L.const_int i8_t 0) addr_end builder);
             addr
         | SListLit(t, l) ->
-                let ltyp = typ_to_ltyp t in
-                let zero = L.const_int i32_t 0 in
-                let one = L.const_int i32_t 1 in
-                let l' = List.map (fun e -> snd e) l in
-                let prefix = "l" in
-                let data = "d" in
-                L.struct_set_body list_t [| L.pointer_type ltyp; L.pointer_type list_t |] false;
-                let first_node_addr = L.build_alloca list_t (prefix ^ "0") builder in
-                let make_node (node_addr, i) e =
-                    let i' = string_of_int i in
-                    (* list_t = { void *data; list_t *next; } *)
-                    (* node -> data *)
-                    let node_data = expr builder e in
-                    let node_data_addr = L.build_alloca ltyp (data ^ i') builder in
-                    ignore(L.build_store node_data node_data_addr builder);
-                    let node_data_gep = L.build_in_bounds_gep node_addr [| zero; zero |] "" builder in
-                    ignore(L.build_store node_data_addr node_data_gep builder);
-                    (* node -> next *)
-                    let i = i + 1 in
-                    let i' = string_of_int i in
-                    let node_addr_ = L.build_in_bounds_gep node_addr [| zero; one |] (prefix ^ i') builder in
-                    let next_node_addr = L.build_alloca list_t (prefix ^ i') builder in
-                    ignore(L.build_store next_node_addr node_addr_ builder);
-                    (next_node_addr, i)
+            let list_t = typ_to_ltyp (A.List(t)) in
+            let zero = L.const_int i32_t 0 in
+            let one = L.const_int i32_t 1 in
+            let l' = List.map (fun e -> snd e) l in
+            let prefix = "l" in
+            let data = "d" in
+            let add_node i e =
+                let addr = L.build_alloca list_t (prefix ^ string_of_int i) builder in
+                (addr, e)
+            in
+            let node_addrs = List.mapi add_node l' in
+            let node_addrs_rev = List.rev node_addrs in
+            let null_addr = L.build_alloca list_t (prefix ^ string_of_int (List.length node_addrs)) builder in
+            ignore(L.build_store (L.const_null (list_t)) null_addr builder);
+            let make_node next_node_addr (node_addr, e) =
+                (* list_t = { void *data; list_t *next; } *)
+                let node_data = expr builder e in
+                let node_data_addr = match t with
+                    A.Int | A.Float | A.Bool | A.String  ->
+                        let addr = L.build_alloca (typ_to_ltyp t) "" builder in
+                        ignore(L.build_store node_data addr builder);
+                        addr
+                    | A.List(_) | A.Dict(_) | _ ->
+                        node_data
                 in
-                let rec traverse_list (node_addr, i) = function
-                    hd::tl ->
-                        traverse_list (make_node (node_addr, i) hd) tl
-                    | [] ->
-                        ignore(L.build_store (L.const_null (list_t)) node_addr builder)
-                in
-                traverse_list (first_node_addr, 0) l';
-                first_node_addr
+                (* node -> data *)
+                let node_data_gep = L.build_in_bounds_gep node_addr [| zero; zero |] "" builder in
+                ignore(L.build_store node_data_addr node_data_gep builder);
+                (* node -> next *)
+                let node_next_gep = L.build_in_bounds_gep node_addr [| zero; one |] "" builder in
+                ignore(L.build_store next_node_addr node_next_gep builder);
+                node_addr
+            in
+            ignore(List.fold_left make_node null_addr node_addrs_rev);
+            fst (List.hd node_addrs)
         | SFunCall("sprint", [sexpr]) ->
             let e = snd sexpr in
             L.build_call printf_func [| str_format; (expr builder e) |] "printf" builder
